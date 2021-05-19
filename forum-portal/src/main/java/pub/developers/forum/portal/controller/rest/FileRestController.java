3541992e34147d723e3a1bb08ea3dd4f16658c04
bianqiangqiang <bianqiangqiang@bytedance.com>
2021-05-19 22:44:09 +0800

    file

--- forum-portal/src/main/java/pub/developers/forum/portal/controller/rest/FileRestController.java
+++ forum-portal/src/main/java/pub/developers/forum/portal/controller/rest/FileRestController.java
@@ -1,5 +1,7 @@
 package pub.developers.forum.portal.controller.rest;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,15 +38,46 @@ public class FileRestController {
     // .css;.js;.png;.jpeg;.jpg;.woff2;.html;.ico;.gif;.bmp;.svg;.woff;.map
     private static final Set<String> ALLOW_TYPES = Sets.newHashSet("png", "jpeg", "jpg", "ico", "gif", "bmp", "svg");
 
+    @PostMapping("/upload-wang-editor")
+    public JSONObject upload0(@RequestParam(value = "image") MultipartFile file, HttpServletRequest request) {
+        String fileName = StringUtil.generateUUID();
+        ResultModel<String> resultModel = uploadFile(file, fileName, request);
+
+        JSONArray urlArr = new JSONArray();
+
+        int errno = 0;
+        if (!resultModel.getSuccess()) {
+            errno = resultModel.getCode();
+        } else {
+            JSONObject url = new JSONObject();
+            url.put("url", resultModel.getData());
+            url.put("alt", fileName);
+            url.put("href", resultModel.getData());
+
+            urlArr.add(url);
+        }
+
+        JSONObject res = new JSONObject();
+        res.put("errno", errno);
+        res.put("data", urlArr);
+        return res;
+    }
+
     @PostMapping("/upload")
-    public ResultModel<String> upload0(@RequestParam(value = "image") MultipartFile file, HttpServletRequest request) {
+    public ResultModel<String> upload(@RequestParam(value = "image") MultipartFile file, HttpServletRequest request) {
+        String fileName = StringUtil.generateUUID();
+        return uploadFile(file, fileName, request);
+    }
+
+    private ResultModel<String> uploadFile(MultipartFile file, String fileName, HttpServletRequest request) {
         request.setAttribute(Constant.REQUEST_HEADER_TOKEN_KEY, WebUtil.cookieGetSid(request));
 
         String fileType = file.getContentType();
-        Boolean isAllowType = false;
+        boolean isAllowType = false;
         for (String allowType : ALLOW_TYPES) {
-            if (fileType.contains(allowType)) {
+            if (fileType != null && fileType.contains(allowType)) {
                 isAllowType = true;
+                break;
             }
         }
         CheckUtil.isFalse(isAllowType, ErrorCodeEn.FILE_UPLOAD_NOT_SUPPORT_IMG_TYPE);
@@ -53,7 +86,7 @@ public class FileRestController {
         try {
             uploadImgRequest = FileUploadImgRequest.builder()
                     .base64(file.getBytes())
-                    .fileName(StringUtil.generateUUID())
+                    .fileName(fileName)
                     .build();
         } catch (Exception e) {
             CheckUtil.isTrue(true, ErrorCodeEn.FILE_UPLOAD_FAIL);
